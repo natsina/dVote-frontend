b7573f85af75b6501b72434c241762d698579693
natsina <norisatsina@gamil.com>
2022-12-04 16:56:42 +0000

    DVote Frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CreateVote from "./CreateVotes";
+import Votes from "./Votes";
+import Navbar from "./Navbar";
+import { useState, useEffect } from "react";
+import { connect, getContract } from "./contract";
 
 function App() {
+  const [contract, setContract] = useState(null);
+  const [connected, setConnected] = useState(false);
+  const [isMember, setIsMember] = useState(false);
+
+  useEffect(() => {
+    window.ethereum.request({ method: "eth_accounts" }).then((accounts) => {
+      if (accounts.length > 0) {
+        handleInit();
+      } else setConnected(false);
+    });
+  }, []);
+
+  const handleInit = () => {
+    setConnected(true);
+    getContract().then(({ contract, signer }) => {
+      setContract(contract);
+
+      if (contract) {
+        signer.getAddress().then((address) => {
+          contract.members(address).then((result) => setIsMember(result));
+        });
+      }
+    });
+  };
+
+  const connectCallback = async () => {
+    const { contract } = await connect();
+    setContract(contract);
+    if (contract) {
+      setConnected(true);
+    }
+  };
+
+  const becomeMember = async () => {
+    if (!contract) {
+      alert("Please connect to metamask.");
+      return;
+    }
+
+    await contract
+      .join()
+      .then(() => {
+        alert("Joined");
+        setIsMember(true);
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar
+        connect={connectCallback}
+        connected={connected}
+        becomeMember={becomeMember}
+        isMember={isMember}
+      />
+      <div className="container">
+        <Routes>
+          <Route
+            path="create-vote"
+            element={<CreateVote contract={contract} />}
+          />
+          <Route path="votes" element={<Votes contract={contract} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
